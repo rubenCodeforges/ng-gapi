c565b8c04d98ad3492b3a7b6cf60867f56bbe807
codeforges <ruben@codeforges.com>
2017-04-27 10:16:00 +0200

    configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./src/googleApi/GoogleApiModule.ts",
+    output: {
+        filename: "bundle.js"
+    },
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
+        fallback: path.join(__dirname, "node_modules")
+    },
+    resolveLoader: {fallback: path.join(__dirname, "node_modules")},
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/, loader: 'ts-loader'
+            }
+        ]
+    },
+    noParse: [
+        path.join(__dirname, 'node_modules', 'zone.js', 'dist')
+    ]
+};
