666fdce7850ce27583793e79824d8e013b49e3ba
codeforges <ruben@codeforges.com>
2017-12-08 11:16:18 +0100

    due to rxjs update , had to fix imports

--- src/GoogleAuthService.ts
+++ src/GoogleAuthService.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
-
 import {Observer} from "rxjs/Observer";
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/mergeMap';
 import {GoogleApiService} from "./GoogleApiService";
 import GoogleAuth = gapi.auth2.GoogleAuth;
 
