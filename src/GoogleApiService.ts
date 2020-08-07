2f438fbb8009d5e76701ddb7f1793645ffdd7fce
Sam Severance <sam.severance@merrillcorp.com>
2020-08-07 10:09:06 -0400

    Prevent Multiple `<script>` Elements From Being Attached
    * Resolves #43

--- src/GoogleApiService.ts
+++ src/GoogleApiService.ts
@@ -9,6 +9,9 @@ export let NG_GAPI_CONFIG: InjectionToken<NgGapiClientConfig> =
 export class GoogleApiService {
   private readonly gapiUrl: string = 'https://apis.google.com/js/api.js';
   private config: GoogleApiConfig;
+  private gapiLoaded: boolean;
+  private node: HTMLScriptElement;
+  private observers: Observer<boolean>[] = [];
 
   constructor(@Inject(NG_GAPI_CONFIG) config: NgGapiClientConfig) {
     this.config = new GoogleApiConfig(config);
@@ -25,14 +28,36 @@ export class GoogleApiService {
 
   private loadGapi(): Observable<boolean> {
     return new Observable((observer: Observer<boolean>) => {
-      const node = document.createElement('script');
-      node.src = this.gapiUrl;
-      node.type = 'text/javascript';
-      document.getElementsByTagName('head')[0].appendChild(node);
-      node.onload = () => {
+      if (this.gapiLoaded) {
         observer.next(true);
         observer.complete();
-      };
+      } else if (!this.node) {
+        /**
+         * script element has not yet been added to document
+         */
+        this.node = document.createElement('script');
+        this.node.async = true;
+        this.node.src = this.gapiUrl;
+        this.node.type = 'text/javascript';
+        this.node.onload = () => {
+          this.gapiLoaded = true;
+          while (this.observers.length) {
+            const observer = this.observers.shift();
+            observer.next(true);
+            observer.complete();
+          }
+          this.node = undefined;
+        };
+        this.node.onerror = () => {
+          this.node = undefined;
+        }
+        document.getElementsByTagName('head')[0].appendChild(this.node);
+      } else {
+        /**
+         * script is in the middle of being loaded
+         */
+        this.observers.push(observer);
+      }
     });
   }
 }
